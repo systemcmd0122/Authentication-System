dff0d8133e6051f186539eeca9d69c8d042ab8d2
systemcmd0122 <systemcmd02.address@gmail.com>
2024-07-20 16:48:52 +0900

    test

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,5 +1,5 @@
 import type { NextAuthOptions } from "next-auth"
-import NextAuth from "next-auth"
+import NextAuth, { Session, User } from "next-auth"
 import CredentialsProvider from 'next-auth/providers/credentials'
 import GoogleProvider from 'next-auth/providers/google'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
