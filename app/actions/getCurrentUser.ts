00b826ae20ffe7db3ab742e21610e607a608efb9
systemcmd0122 <systemcmd02.address@gmail.com>
2024-07-21 13:56:29 +0900

    test

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth'
-import { authOptions } from '@/app/lib/auth'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import prisma from '@/app/lib/prisma'
 
 //ログインユーザー取得
